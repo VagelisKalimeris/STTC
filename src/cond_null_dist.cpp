01148a7ec81a7c36ecfb098aaf430d429dd7f610
VagelisKalimeris <42414723+VagelisKalimeris@users.noreply.github.com>
2018-08-18 22:24:10 +0300

    Add files via upload

---
+++ src/cond_null_dist.cpp
@@ -0,0 +1,63 @@
+/******************************************************************************
+*******************************************************************************
+*                                                                             *
+* PROJECT NAME: STTC Analyses                                                 *
+*                                                                             *
+* FILE NAME: cond_null_dist.cpp                                               *
+*                                                                             *
+*******************************************************************************
+******************************************************************************/
+
+
+
+#include <cmath>
+#include<vector>
+using namespace std;
+
+/******************************************************************************
+* FUNCTION NAME: circ_STTC_A_B_C                                              *
+*                                                                             *
+* ARGUMENTS: A pre-existing array to store the results. Three neuron's        *
+*             timelines(references to vectors), and a time interval(int).     *
+*                                                                             *
+* PURPOSE: Calculates circ_shifts_num random STTC values.                     *
+*                                                                             *
+* RETURNS: None.                                                              *
+*                                                                             *
+* I/O: None.                                                                  *
+*                                                                             *
+******************************************************************************/
+void circ_STTC_A_B_C(double results_arr[], int circ_shifts_num, 
+	            const vector<int> &time_line_A, const vector<int> &time_line_B, 
+	                                    const vector<int> &time_line_C, int Dt)
+{
+	if (sign_trpl_limit(time_line_A, time_line_C)) {
+		for (int i = 0; i < circ_shifts_num; i++) {
+			vector<int> to_shift = time_line_C;
+
+			circular_shift(to_shift, circ_shifts_num);
+			results_arr[i] = STTC_AB_C(time_line_A, time_line_B, to_shift, Dt);
+		}
+	}
+}
+
+
+/******************************************************************************
+* FUNCTION NAME: sign_trpl_limit                                              *
+*                                                                             *
+* ARGUMENTS: Two neuron's timelines(references to vectors), and a time        *
+*             interval(int).                                                  *
+*                                                                             *
+* PURPOSE: Calculates if the number of firing events of ‘reduced A’ is        *
+*           greater than 5 or not.                                            *
+*                                                                             *
+* RETURNS: True or False.                                                     *
+*                                                                             *
+* I/O: None.                                                                  *
+*                                                                             *
+******************************************************************************/
+bool sign_trpl_limit(const vector<int> &time_line_A, 
+                                        const vector<int> &time_line_C, int Dt)
+{
+
+}
