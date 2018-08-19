1afa965237b40e697ac19d8102d32f553bfc0a9b
VagelisKalimeris <42414723+VagelisKalimeris@users.noreply.github.com>
2018-08-20 00:31:04 +0300

    Update cond_null_dist.cpp

--- src/cond_null_dist.cpp
+++ src/cond_null_dist.cpp
@@ -58,5 +58,32 @@ void circ_STTC_A_B_C(double results_arr[], int circ_shifts_num,
 bool sign_trpl_limit(const vector<int> &time_line_A, 
                                         const vector<int> &time_line_C, int Dt)
 {
-// To Do..
-}
+    int s = 0, a = 0, c = 0;
+    
+    /* all spikes of A are before tiles of C */
+    if(time_line_A.back() < time_line_C.front()) {
+        return false;
+    }
+    /* all spikes of A are after tiles of C */
+    if((time_line_C.back() + Dt) < time_line_A.front()) {
+        return false;
+    }
+    
+    while((a < time_line_A.size()) && (c < time_line_C.size())) {
+        /* spike of A is within tile of spike of C [tC, tC + Dt] */
+        if((time_line_A[a] >= time_line_C[c]) && 
+                                 (time_line_A[a] <= (time_line_C[c] + Dt))) {
+            s++;
+            a++;
+        }
+        /* spike of A is before tile of spike of C [tC, tC + Dt] */
+        else if(time_line_A[a] < time_line_C[c]) {
+            a++;
+        }
+        /* spike of A is after tile of spike of C [tC, tC + Dt] */
+        else if(time_line_A[a] > (time_line_C[c] + Dt)) {
+            c++;
+        }
+    }
+    
+    return (s > 5);}
